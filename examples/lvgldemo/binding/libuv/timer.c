9bf39c9d98a66baae297dd537b89a5a47320bd0c
Junkang Yuan <yuanjunkang@gmail.com>
2025-07-31 14:20:56 +0800

    Add a comment and look forward to the libuv asynchronous process in a multi-threaded environment.

--- examples/lvgldemo/binding/libuv/timer.c
+++ examples/lvgldemo/binding/libuv/timer.c
@@ -6,9 +6,20 @@
 
 static void rust_wake_and_poll(uv_timer_t *handle)
 {
+    // If a multi-threaded implementation is needed in the future,
+    // meaning the callback is not called from the libuv thread,
+    // it is necessary to add the state to the thread-safe queue,
+    // and then send uv_async_send to consume in the callback
+    // and wake up the corresponding Future.
+    //
+    // However, the limit of reusing uv_async_t is the same type of Future,
+    // because different types of Future have different wake-up functions,
+    // just like rust_delay_wake.
     void rust_delay_wake(void *);
     rust_delay_wake(uv_handle_get_data((uv_handle_t *)handle));
 
+    // Similarly, waking up the Executor in multi-threaded mode also requires
+    // uv_async_t support, but this has already been correctly implemented.
     int rust_executor_wake(void);
     int ret = rust_executor_wake();
     if (ret < 0)
