b10c97af5047dc346286e25f13fa408e6cfaf82d
Junkang Yuan <yuanjunkang@gmail.com>
2025-08-29 16:25:52 +0800

    Refactored Delay to use stack allocation in non-cancellation mode to improve performance.

--- examples/rust/hello/src/runtime/delay.rs
+++ examples/rust/hello/src/runtime/delay.rs
@@ -13,44 +13,20 @@ use async_cancellation_token::{CancellationToken, Cancelled, CancelledFuture};
 use crate::{binding::libuv::UvTimer, runtime::UI_LOOP};
 
 pub struct Delay {
-    state: Rc<RefCell<State>>,
+    state: State,
     duration: Duration,
-    cancel_future: Option<CancelledFuture>,
-}
-
-#[derive(Debug, Default)]
-enum DelayState {
-    /// The delay is still pending.
-    /// - The `uv_timer_t` may be running (or about to be started).
-    /// - The waker may be set to wake the current task when the timer completes.
-    /// - Cancellation may occur at any time.
-    #[default]
-    Pending,
-
-    /// The delay has completed naturally (the timer fired).
-    /// - The `uv_timer_t` callback has executed `rust_delay_wake()`.
-    /// - The future should return `Ok(())` when polled.
-    /// - Any further poll calls will observe `Completed` and immediately return `Ok(())`.
-    Completed,
-
-    /// The delay has been cancelled via a `CancellationToken`.
-    /// - The registered cancellation callback may have stopped the timer.
-    /// - The `CancelledFuture` will be ready, so poll should return `Err(Cancelled)`.
-    /// - The future is considered terminated, and no further waiting should occur.
-    Cancelled,
 }
 
 // Shared state between the future and the `uv_timer_t`
+enum State {
+    StackAllocated(InnerState),
+    Cancellable(Rc<RefCell<InnerState>>, CancelledFuture),
+}
+
 #[derive(Debug, Default)]
-struct State {
+struct InnerState {
     /// Current status of the delay.
-    ///
-    /// - `Pending`: The timer is running or about to start, the future is waiting.
-    /// - `Completed`: The timer has fired, the future should return `Ok(())`.
-    /// - `Cancelled`: Cancellation was triggered, the future should return `Err(Cancelled)`.
-    ///
-    /// This field drives the logic in `Delay::poll` and `rust_delay_wake`.
-    state: DelayState,
+    state: PollState,
 
     // The waker for the task that `Delay` is running on.
     // The `uv_timer_t` can use this after setting `completed = true`
@@ -62,13 +38,19 @@ struct State {
     handle: Option<UvTimer>,
 }
 
+#[derive(Debug, Default, Copy, Clone)]
+enum PollState {
+    #[default]
+    Pending,
+    Completed,
+    Cancelled,
+}
+
 impl Future for Delay {
     type Output = Result<(), Cancelled>;
 
     fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
         let this = self.get_mut();
-
-        let mut s = this.state.borrow_mut();
 
         // 1) Poll the cancellation future (if any) first.
         //
@@ -89,12 +71,11 @@ impl Future for Delay {
         // If ready, mark the state as Cancelled and return `Err(Cancelled)` immediately.
         // Note: dropping the handle early is optional, since Delay will drop it
         // automatically when it goes out of scope.
-
-        if let Some(fut) = &mut this.cancel_future {
+        if let State::Cancellable(rc_inner_state, fut) = &mut this.state {
+            let mut s = rc_inner_state.borrow_mut();
             if let Poll::Ready(()) = Pin::new(fut).poll(cx) {
-                assert!(matches!(s.state, DelayState::Pending));
-
-                s.state = DelayState::Cancelled;
+                assert!(matches!(s.state, PollState::Pending));
+                s.state = PollState::Cancelled;
                 return Poll::Ready(Err(Cancelled));
             }
         }
@@ -102,24 +83,27 @@ impl Future for Delay {
         // 2) Now do the rest under a single borrow_mut to reduce multiple borrows.
         // Create the uv timer only when the delay is still Pending and no handle exists.
 
+        let state_ptr = &mut this.state as *mut _ as *mut _;
+        let s = match &mut this.state {
+            State::StackAllocated(inner_state) => inner_state,
+            State::Cancellable(rc_inner_state, _) => &mut *rc_inner_state.borrow_mut(),
+        };
+
         // Working with the `uv_timer_t`
-        if s.handle.is_none() {
+        s.handle.get_or_insert_with(|| {
             // Ensure the timer is only created once for this Delay
-            assert!(matches!(s.state, DelayState::Pending));
+            assert!(matches!(s.state, PollState::Pending));
 
             let uv_timer = UvTimer::new(unsafe { UI_LOOP });
-            uv_timer.start(
-                this.duration.as_millis() as u64,
-                Rc::as_ptr(&this.state) as *mut _,
-            );
-            s.handle.replace(uv_timer);
-        }
+            uv_timer.start(this.duration.as_millis() as u64, state_ptr);
+            uv_timer
+        });
 
         // Look at the shared state to see if the timer has already completed.
         match s.state {
-            DelayState::Completed => Poll::Ready(Ok(())),
-            DelayState::Cancelled => Poll::Ready(Err(Cancelled)),
-            DelayState::Pending => {
+            PollState::Completed => Poll::Ready(Ok(())),
+            PollState::Cancelled => Poll::Ready(Err(Cancelled)),
+            PollState::Pending => {
                 // Set waker so that the `uv_timer_t` can wake up the current task
                 // when the timer has completed, ensuring that the future is polled
                 // again and sees that `completed = true`.
@@ -148,9 +132,8 @@ impl Delay {
     // Create a new `Delay` which will complete after the specified duration elapses.
     pub fn new(duration: Duration) -> Self {
         Delay {
-            state: Rc::new(RefCell::new(State::default())),
+            state: State::StackAllocated(Default::default()),
             duration,
-            cancel_future: None,
         }
     }
 
@@ -158,7 +141,8 @@ impl Delay {
     // either when the specified duration elapses
     // or when the associated `CancellationToken` is cancelled.
     pub fn new_with_token(duration: Duration, token: CancellationToken) -> Self {
-        let state = Rc::new(RefCell::new(State::default()));
+        let state =
+            State::Cancellable(Rc::new(RefCell::new(Default::default())), token.cancelled());
 
         // Register a cancellation callback with the token.
         //
@@ -170,36 +154,40 @@ impl Delay {
         // runs registered callbacks (like this stop closure) and then wakes token.wakers.
         // The CancelledFuture registers this task's waker with the token, so when
         // cancel() calls wake() the task will be resumed and can observe the Cancelled state.
-        let weak_state = Rc::downgrade(&state);
-        token.register(move || {
-            if let Some(state) = weak_state.upgrade() {
-                if let Some(handle) = state.borrow().handle.as_ref() {
-                    handle.cancel();
-                }
+        match &state {
+            State::StackAllocated(_) => unreachable!(),
+            State::Cancellable(state, _) => {
+                let weak_state = Rc::downgrade(state);
+                token.register(move || {
+                    if let Some(state) = weak_state.upgrade() {
+                        if let Some(handle) = state.borrow().handle.as_ref() {
+                            handle.cancel();
+                        }
+                    }
+                });
             }
-        });
-
-        Delay {
-            state,
-            duration,
-            cancel_future: Some(token.cancelled()),
         }
+
+        Delay { state, duration }
     }
 }
 
 #[no_mangle]
 extern "C" fn rust_delay_wake(state: *mut c_void) {
     assert!(!state.is_null());
-    let state = unsafe { &*(state as *mut RefCell<State>) };
+    let state = unsafe { &mut *(state as *mut State) };
 
-    let mut s = state.borrow_mut();
+    let inner_state = match state {
+        State::StackAllocated(inner_state) => inner_state,
+        State::Cancellable(rc_inner_state, _) => &mut *rc_inner_state.borrow_mut(),
+    };
 
-    assert!(matches!(s.state, DelayState::Pending));
+    assert!(matches!(inner_state.state, PollState::Pending));
 
     // Signal that the timer has completed and wake up the last
     // task on which the future was polled, if one exists.
-    s.state = DelayState::Completed;
-    if let Some(waker) = s.waker.take() {
+    inner_state.state = PollState::Completed;
+    if let Some(waker) = inner_state.waker.take() {
         waker.wake()
     }
 }
